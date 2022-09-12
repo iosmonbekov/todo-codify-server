70ef16c3c791fb66ad546e7d94d87888f14c8f41
Islam Osmonbekov <islam.o@veles.finance>
2022-09-12 19:54:02 +0600

    fix

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
